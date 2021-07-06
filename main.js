53b970a77fb2adda1fa7f57c74403907369dc527
justinalequin <justinalequin@gmail.com>
2021-07-06 09:48:41 -0400

    All changes made with JS.

---
+++ main.js
@@ -0,0 +1,40 @@
+ const p = document.querySelector('p');
+ p.style.color = 'lightblue';
+ p.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+
+ const h = document.querySelector('h1');
+ h.style.fontSize = '48px'
+
+const third = document.querySelector('#item-3');
+third.innerText = 'I say, "Hi!"'
+
+ const thirteen = document.querySelector('#item-13');
+ thirteen.style.opacity = "0.5";
+
+ const img = document.querySelector('img')
+ img.src = 'trex_4.jpg';
+ img.style.height = '300px';
+
+ const sky = document.querySelector('#sky')
+ sky.style.height = '300px';
+
+ const sixteen = document.querySelector('#item-16');
+ sixteen.className +="item"
+// sixteen.id = 'item-16'
+ sixteen.innerText = 'Won\'t get fooled again';
+
+
+
+
+
+
+
+
+
+
+ //img.style.width = '300px';
+
+// const newPic = document.querySelector('sky')
+// newPic.style.height = '300px';
+ 
+ 
